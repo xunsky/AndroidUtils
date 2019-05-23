a362e979b6bc646157b68ebccba0cadf49888647
weixun.lwx <weixun.lwx@ruishanio.com>
2019-05-23 11:26:08 +0800

    解决部分手机关闭toast后无法显示toast的问题

--- app/src/main/java/xunsky/utils/android_utils/toast/ToastUtils.java
+++ app/src/main/java/xunsky/utils/android_utils/toast/ToastUtils.java
@@ -1,22 +1,36 @@
 package xunsky.utils.android_utils.toast;
 
+import android.app.Application;
 import android.view.Gravity;
 import android.widget.Toast;
 
 import xunsky.utils.context_provider.ContextProvider;
 
 public class ToastUtils {
+    private static boolean isSupport=false;
+    /*
+    * 部分手机关闭通知后无法显示通知
+    * 开启支持让其可以显示toast
+    * */
+    private static void useSupport(Application app){
+        com.hjq.toast.ToastUtils.init(app);
+        isSupport=true;
+    }
     public static Toast sToast;
     public static void toast(String message){
-        if (sToast==null){
-            synchronized (ToastUtils.class){
-                if (sToast==null){
-                    sToast=Toast.makeText(ContextProvider.get(), "", Toast.LENGTH_LONG);
+        if (!isSupport){
+            if (sToast==null){
+                synchronized (ToastUtils.class){
+                    if (sToast==null){
+                        sToast=Toast.makeText(ContextProvider.get(), "", Toast.LENGTH_LONG);
 //                    sToast.setGravity(Gravity.CENTER, 0,0);
+                    }
                 }
             }
+            sToast.setText(message);
+            sToast.show();
+        }else{
+            com.hjq.toast.ToastUtils.show(message);
         }
-        sToast.setText(message);
-        sToast.show();
     }
 }
