bee019710370b45d6fbc5bc0a385c2ed3df43ea3
weixun.lwx <weixun.lwx@ruishanio.com>
2019-08-02 18:13:22 +0800

    upate

--- app/src/main/java/xunsky/utils/android_utils/json/JsonUtils.java
+++ app/src/main/java/xunsky/utils/android_utils/json/JsonUtils.java
@@ -7,7 +7,9 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +23,13 @@ public class JsonUtils {
         return sGson.fromJson(json,clazz);
     }
 
+
+    /*
+     List<Photo> cachedPhotos = new Gson().fromJson(
+        snapshot.getString(VALUE_INDEX),
+        new TypeToken<List<Photo>>(){}.getType()
+    );
+     */
     public static <T> List<T> parseJsonList(String json, Class<T> clazz){
         //如下处理会在get(position)时 com.google.gson.internal.LinkedTreeMap cannot be cast to
         //真实类型为linkedTreeMap 无法get
@@ -28,10 +37,6 @@ public class JsonUtils {
         JsonArray arry = new JsonParser().parse(json).getAsJsonArray();
         for (JsonElement jsonElement : arry) {
             list.add(sGson.fromJson(jsonElement, clazz));
-        }
-        for (T t: list) {
-            Log.d("meee",Thread.currentThread().getStackTrace()[2].getClassName()+"."+Thread.currentThread().getStackTrace()[2].getLineNumber()+"\n"
-                    +t.getClass());
         }
         return list;
     }
